34a6ec186330cc8e4b8c0019d513fa2f1ea93e40
ibrusniak <ivan.brusniak@gmail.com>
2026-08-31 11:18:57 +0200

    Add button1 listener

--- app/src/main/java/com/ibrusniak/app/MainActivity.kt
+++ app/src/main/java/com/ibrusniak/app/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.ibrusniak.app
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button1: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initLateInitVars()
+        setOnClickListeners()
+    }
+
+    private fun initLateInitVars() {
+        button1 = findViewById(R.id.button1)
+    }
+
+    private fun setOnClickListeners() {
+        button1.setOnClickListener(::button1OnClickListener)
+    }
+
+    private fun button1OnClickListener(v: View) {
+
     }
 }
