493d3e7cceba7733a044ac71f41b5354b103e776
ibrusniak <ivan.brusniak@gmail.com>
2026-08-31 17:05:06 +0200

    Version 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ibrusniak.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1"
+        versionCode = 2
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
